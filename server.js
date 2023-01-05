021df81e0df7fe9e6f766e37f6922ff6f2f44a4a
Tuan <tuanqxktk5@outlook.com>
2023-01-05 20:29:31 +0700

    update cors

--- server.js
+++ server.js
@@ -17,12 +17,6 @@ app.use(
     extended: true,
   })
 );
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.get("/", (req, res) => {
   res.json({
